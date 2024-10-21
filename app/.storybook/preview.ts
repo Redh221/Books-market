1ee570a4a3560a88a77c938df50dc378b1b2884d
Redh221 <denis.markin.95@gmail.com>
2024-10-21 18:20:59 +0400

    added_storybook_once_again

--- app/.storybook/preview.ts
+++ app/.storybook/preview.ts
@@ -2,7 +2,6 @@ import type { Preview } from "@storybook/react";
 import { withRouter } from "storybook-addon-remix-react-router";
 import { reactRouterParameters } from "storybook-addon-remix-react-router";
 
-// Подключаем Google Fonts через JavaScript
 const link = document.createElement("link");
 link.href =
   "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap";
@@ -24,6 +23,6 @@ export default {
   decorators: [withRouter],
   parameters: {
     reactRouter: reactRouterParameters({}),
-    ...preview.parameters, // Чтобы параметры контролов также применились
+    ...preview.parameters,
   },
 } satisfies Preview;
